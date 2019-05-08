58a0c036082a0914c68ab72db1e26c70902ebc12
faluna <jlyvgxs9001@icloud.com>
2019-05-09 00:26:05 +0900

    Fix: fix fzf config

--- fzf/fzf.zsh
+++ fzf/fzf.zsh
@@ -1,16 +1,17 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/faluna/.cache/dein/repos/github.com/junegunn/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/Users/faluna/.cache/dein/repos/github.com/junegunn/fzf/bin"
+if [[ ! "$PATH" == */$HOME/.cache/dein/repos/github.com/junegunn/fzf/bin* ]]; then
+# if [[ ! "$PATH" == */Users/faluna/.cache/dein/repos/github.com/junegunn/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/$HOME/.cache/dein/repos/github.com/junegunn/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/Users/faluna/.cache/dein/repos/github.com/junegunn/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.cache/dein/repos/github.com/junegunn/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/Users/faluna/.cache/dein/repos/github.com/junegunn/fzf/shell/key-bindings.zsh"
+source "$HOME/.cache/dein/repos/github.com/junegunn/fzf/shell/key-bindings.zsh"
 
 export FZF_CTRL_T_COMMAND="rg --files --hidden --follow --glob '!.git/*' 2>/dev/null"
 export FZF_CTRL_T_OPTS='--height 80% --border --layout=reverse --preview "bat --color=always --style=header,grid --line-range :100 {}"'
