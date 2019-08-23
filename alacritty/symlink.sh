c883704c1234526431ee8924fa0ab0c69e8af04e
faluna <jlyvgxs9001@icloud.com>
2019-08-23 09:46:54 +0900

    Fix: fix alacritty.yml file
    alacrittyのconfig file
    の項目が新しくなったので，そのスタイルに対応したファイルを追加

--- alacritty/symlink.sh
+++ alacritty/symlink.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # alacritty config deirectory
-# mkdir -p $HOME/.config/alacritty
+mkdir -p $HOME/.config/alacritty
 
-# if [ ! -h "${XDG_CONFIG_HOME:-$HOME/.config}/alacritty/alacritty.yml" ]; then
-  # ln -s "$HOME/dotfiles/alacritty/alacritty.yml" "${XDG_CONFIG_HOME:-$HOME/.config}/alacritty/alacritty.yml"
-# fi
+if [ ! -h "${XDG_CONFIG_HOME:-$HOME/.config}/alacritty/alacritty.yml" ]; then
+  ln -s "$HOME/dotfiles/alacritty/alacritty.yml" "${XDG_CONFIG_HOME:-$HOME/.config}/alacritty/alacritty.yml"
+fi
 
 if [ ! -h "$HOME/.alacritty.yml" ]; then
   ln -s "$HOME/dotfiles/alacritty/alacritty.yml" "$HOME/.alacritty.yml"
