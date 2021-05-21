95248b20ca9b170ff590bc014b3582af3b03767a
faluna <37044099+faluna@users.noreply.github.com>
2021-05-21 16:42:28 +0900

    add:tada: Make gitconfig for snowhite

--- gitFile/symlink.sh
+++ gitFile/symlink.sh
@@ -9,3 +9,8 @@ if [ ! -h "$HOME/.commit_template" ]
 then
   ln -s "dotfiles/gitFile/commit_template" "$HOME/.commit_template"
 fi
+
+if [ ! -h "$HOME/.gitconfig_snowhite" ]
+then
+  ln -s "dotfiles/gitFile/gitconfig_snowhite" "$HOME/.gitconfig_snowhite"
+fi
