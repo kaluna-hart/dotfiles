1c2e9c99dc782e29d4b60546ee649f5aa6449d46
faluna <37044099+faluna@users.noreply.github.com>
2022-08-27 17:01:59 +0900

    feat:sparkles: Add python linter and formattter

--- nvim/plugin/null-ls.rc.lua
+++ nvim/plugin/null-ls.rc.lua
@@ -1,28 +1,36 @@
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local status, null_ls = pcall(require, "null-ls")
-if (not status) then return end
+if not status then
+	return
+end
 
-
-null_ls.setup {
-  sources = {
-    null_ls.builtins.formatting.prettier.with {
-      prefer_local = "node_modules/.bin",
-    },
-    null_ls.builtins.code_actions.gitsigns,
-    null_ls.builtins.formatting.stylua,
-  },
-  -- you can reuse a shared lspconfig on_attach callback here
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-                    vim.lsp.buf.formatting_sync()
-                end,
-            })
-        end
-    end,
-}
+null_ls.setup({
+	sources = {
+		null_ls.builtins.formatting.prettier.with({
+			prefer_local = "node_modules/.bin",
+		}),
+		null_ls.builtins.code_actions.gitsigns,
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.diagnostics.pylint.with({
+			diagnostics_postprocess = function(diagnostic)
+				diagnostic.code = diagnostic.message_id
+			end,
+		}),
+		null_ls.builtins.formatting.isort,
+		null_ls.builtins.formatting.black,
+	},
+	-- you can reuse a shared lspconfig on_attach callback here
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+					vim.lsp.buf.formatting_sync()
+				end,
+			})
+		end
+	end,
+})
