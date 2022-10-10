bcf0aef0423e00dbf13802793e1a4f46bd000016
faluna <37044099+faluna@users.noreply.github.com>
2022-10-10 09:18:12 +0900

    fix:+1: vistaにnvim-lspを適用

--- nvim/plugin/null-ls.rc.lua
+++ nvim/plugin/null-ls.rc.lua
@@ -11,11 +11,11 @@ null_ls.setup({
 		}),
 		null_ls.builtins.code_actions.gitsigns,
 		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.diagnostics.pylint.with({
-			diagnostics_postprocess = function(diagnostic)
-				diagnostic.code = diagnostic.message_id
-			end,
-		}),
+		--   null_ls.builtins.diagnostics.pylint.with({
+		-- 	diagnostics_postprocess = function(diagnostic)
+		-- 		diagnostic.code = diagnostic.message_id
+		-- 	end,
+		-- }),
 		null_ls.builtins.formatting.isort,
 		null_ls.builtins.formatting.black,
 	},
