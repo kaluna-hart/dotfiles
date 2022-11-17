cc0b434e8c6715039e1ada8e8b4d2f01efc7630c
faluna <37044099+faluna@users.noreply.github.com>
2022-11-18 00:35:44 +0900

    nvim用プラグインファイルのアップデート

--- nvim/after/plugin/mason.rc.lua
+++ nvim/after/plugin/mason.rc.lua
@@ -1,10 +1,10 @@
 local status, mason = pcall(require, "mason")
 if not status then
-	return
+  return
 end
 local status2, mason_lspconfig = pcall(require, "mason-lspconfig")
 if not status2 then
-	return
+  return
 end
 
 mason.setup()
@@ -12,38 +12,38 @@ mason.setup()
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-	-- LSPサーバーのフォーマット機能を無効にするには下の行をコメントアウト
-	-- 例えばtypescript-language-serverにはコードのフォーマット機能が付いているが代わりにprettierでフォーマットしたいときなどに使う
-	client.resolved_capabilities.document_formatting = false
+  -- LSPサーバーのフォーマット機能を無効にするには下の行をコメントアウト
+  -- 例えばtypescript-language-serverにはコードのフォーマット機能が付いているが代わりにprettierでフォーマットしたいときなどに使う
+  client.server_capabilities.document_formatting = false
 
-	-- Mappings.
-	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	local bufopts = { noremap = true, silent = true, buffer = bufnr }
-	vim.keymap.set("n", "<leader>lD", vim.lsp.buf.declaration, bufopts)
-	-- vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition, bufopts)
-	-- vim.keymap.set("n", "<leader>lh", vim.lsp.buf.hover, bufopts)
-	vim.keymap.set("n", "<leader>li", vim.lsp.buf.implementation, bufopts)
-	-- vim.keymap.set("n", "<leader>ls", vim.lsp.buf.signature_help, bufopts)
-	vim.keymap.set("n", "<leader>lwa", vim.lsp.buf.add_workspace_folder, bufopts)
-	vim.keymap.set("n", "<leader>lwr", vim.lsp.buf.remove_workspace_folder, bufopts)
-	vim.keymap.set("n", "<leader>lwl", function()
-		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-	end, bufopts)
-	vim.keymap.set("n", "<leader>lt", vim.lsp.buf.type_definition, bufopts)
-	-- vim.keymap.set("n", "<leader>lrn", vim.lsp.buf.rename, bufopts)
-	-- vim.keymap.set("n", "<leader>lc", vim.lsp.buf.code_action, bufopts)
-	vim.keymap.set("n", "<leader>lr", vim.lsp.buf.references, bufopts)
-	vim.keymap.set("n", "<leader>lf", vim.lsp.buf.formatting, bufopts)
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+  vim.keymap.set("n", "<leader>lD", vim.lsp.buf.declaration, bufopts)
+  -- vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition, bufopts)
+  -- vim.keymap.set("n", "<leader>lh", vim.lsp.buf.hover, bufopts)
+  vim.keymap.set("n", "<leader>li", vim.lsp.buf.implementation, bufopts)
+  -- vim.keymap.set("n", "<leader>ls", vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set("n", "<leader>lwa", vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set("n", "<leader>lwr", vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set("n", "<leader>lwl", function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set("n", "<leader>lt", vim.lsp.buf.type_definition, bufopts)
+  -- vim.keymap.set("n", "<leader>lrn", vim.lsp.buf.rename, bufopts)
+  -- vim.keymap.set("n", "<leader>lc", vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set("n", "<leader>lr", vim.lsp.buf.references, bufopts)
+  vim.keymap.set("n", "<leader>lf", function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 mason_lspconfig.setup()
 mason_lspconfig.setup_handlers({
-	function(server_name) -- default handler (optional)
-		require("lspconfig")[server_name].setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-		})
-	end,
+  function(server_name) -- default handler (optional)
+    require("lspconfig")[server_name].setup({
+      on_attach = on_attach,
+      capabilities = capabilities,
+    })
+  end,
 })
--- nvim/plugin/null-ls.rc.lua
+++ nvim/plugin/null-ls.rc.lua
@@ -1,39 +1,56 @@
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local status, null_ls = pcall(require, "null-ls")
 if not status then
-	return
+  return
 end
 
+-- local mason_null_ls = require("mason-null-ls")
+-- mason_null_ls.setup({
+-- 	ensure_installed = { "stylua", "jq", "prettierd" },
+-- })
+
+-- mason_null_ls.setup_handlers({
+-- 	function(source_name)
+-- 		-- all sources with no handler get passed here
+-- 	end,
+-- 	stylua = function()
+-- 		null_ls.register(null_ls.builtins.formatting.stylua)
+-- 	end,
+-- 	jq = function()
+-- 		null_ls.register(null_ls.builtins.formatting.jq)
+-- 	end,
+-- })
+
 null_ls.setup({
-	sources = {
-		null_ls.builtins.formatting.prettier.with({
-			prefer_local = "node_modules/.bin",
-		}),
-		null_ls.builtins.code_actions.gitsigns,
-		null_ls.builtins.formatting.stylua,
-		--   null_ls.builtins.diagnostics.pylint.with({
-		-- 	diagnostics_postprocess = function(diagnostic)
-		-- 		diagnostic.code = diagnostic.message_id
-		-- 	end,
-		-- }),
-		null_ls.builtins.formatting.isort, -- for python
-		null_ls.builtins.formatting.black, -- for python
-		null_ls.builtins.formatting.taplo, -- for toml
-		null_ls.builtins.diagnostics.hadolint, -- for Dockerfile
-		null_ls.builtins.formatting.yamlfmt, -- for yaml
-	},
-	-- you can reuse a shared lspconfig on_attach callback here
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-					vim.lsp.buf.formatting_sync()
-				end,
-			})
-		end
-	end,
+  sources = {
+    null_ls.builtins.formatting.prettier.with({
+      prefer_local = "node_modules/.bin",
+    }),
+    null_ls.builtins.code_actions.gitsigns,
+    null_ls.builtins.formatting.stylua,
+    --   null_ls.builtins.diagnostics.pylint.with({
+    -- 	diagnostics_postprocess = function(diagnostic)
+    -- 		diagnostic.code = diagnostic.message_id
+    -- 	end,
+    -- }),
+    null_ls.builtins.formatting.isort, -- for python
+    null_ls.builtins.formatting.black, -- for python
+    null_ls.builtins.formatting.taplo, -- for toml
+    null_ls.builtins.diagnostics.hadolint, -- for Dockerfile
+    null_ls.builtins.formatting.yamlfmt, -- for yaml
+  },
+  -- you can reuse a shared lspconfig on_attach callback here
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      -- vim.api.nvim_create_autocmd("BufWritePre", {
+      -- 	group = augroup,
+      -- 	buffer = bufnr,
+      -- 	callback = function()
+      -- 		-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+      -- 		vim.lsp.buf.format()
+      -- 	end,
+      -- })
+    end
+  end,
 })
