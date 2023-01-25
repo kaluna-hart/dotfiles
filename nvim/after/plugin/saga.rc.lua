621ea9d04c6ab40d2870e4af8f71814548bc70bf
kaluna-hart <37044099+kaluna-hart@users.noreply.github.com>
2023-01-26 01:30:47 +0900

    feat:sparkles: saga設定更新

--- nvim/after/plugin/saga.rc.lua
+++ nvim/after/plugin/saga.rc.lua
@@ -1,158 +1,81 @@
 local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
-local saga = require("lspsaga")
-
-saga.init_lsp_saga({
-  -- Options with default value
-  -- "single" | "double" | "rounded" | "bold" | "plus"
-  border_style = "single",
-  --the range of 0 for fully opaque window (disabled) to 100 for fully
-  --transparent background. Values between 0-30 are typically most useful.
-  saga_winblend = 20,
-  -- when cursor in saga window you config these to move
-  move_in_saga = { prev = "<C-p>", next = "<C-n>" },
-  -- Error, Warn, Info, Hint
-  -- use emoji like
-  -- { "🙀", "😿", "😾", "😺" }
-  -- or
-  -- { "😡", "😥", "😤", "😐" }
-  -- and diagnostic_header can be a function type
-  -- must return a string and when diagnostic_header
-  -- is function type it will have a param `entry`
-  -- entry is a table type has these filed
-  -- { bufnr, code, col, end_col, end_lnum, lnum, message, severity, source }
-  diagnostic_header = { " ", " ", " ", "ﴞ " },
-  -- preview lines of lsp_finder and definition preview
-  max_preview_lines = 10,
-  -- use emoji lightbulb in default
-  code_action_icon = "💡",
-  -- if true can press number to execute the codeaction in codeaction window
-  code_action_num_shortcut = true,
-  -- same as nvim-lightbulb but async
-  code_action_lightbulb = {
-    enable = true,
-    enable_in_insert = false,
-    cache_code_action = true,
-    sign = false,
-    update_time = 150,
-    sign_priority = 20,
-    virtual_text = false,
+local saga = require("lspsaga").setup({
+  lightbulb = {
+    enable = false,
+    enable_in_insert = true,
+    sign = true,
+    sign_priority = 40,
+    virtual_text = true,
   },
-  -- finder icons
-  finder_icons = {
-    def = "  ",
-    ref = "諭 ",
-    link = "  ",
-  },
-  -- finder do lsp request timeout
-  -- if your project big enough or your server very slow
-  -- you may need to increase this value
-  finder_request_timeout = 1500,
-  finder_action_keys = {
-    open = "o",
-    vsplit = "s",
-    split = "i",
-    tabe = "t",
-    quit = "q",
-  },
-  code_action_keys = {
-    quit = "q",
-    exec = "<CR>",
-  },
-  definition_action_keys = {
-    edit = "<C-c>o",
-    vsplit = "<C-c>v",
-    split = "<C-c>i",
-    tabe = "<C-c>t",
-    quit = "q",
-  },
-  rename_action_quit = "<C-c>",
-  rename_in_select = true,
-  -- show symbols in winbar must nightly
-  -- in_custom mean use lspsaga api to get symbols
-  -- and set it to your custom winbar or some winbar plugins.
-  -- if in_cusomt = true you must set in_enable to false
-  -- symbol_in_winbar = {
-  -- 	in_custom = false,
-  -- 	enable = true,
-  -- 	separator = " ",
-  -- 	show_file = true,
-  -- 	-- define how to customize filename, eg: %:., %
-  -- 	-- if not set, use default value `%:t`
-  -- 	-- more information see `vim.fn.expand` or `expand`
-  -- 	-- ## only valid after set `show_file = true`
-  -- 	file_formatter = "",
-  -- 	click_support = false,
-  -- },
-  -- show outline
-  show_outline = {
-    win_position = "right",
-    --set special filetype win that outline window split.like NvimTree neotree
-    -- defx, db_ui
-    win_with = "",
-    win_width = 30,
-    auto_enter = true,
-    auto_preview = true,
-    virt_text = "┃",
-    jump_key = "o",
-    -- auto refresh when change buffer
-    auto_refresh = true,
-  },
-  -- custom lsp kind
-  -- usage { Field = 'color code'} or {Field = {your icon, your color code}}
-  custom_kind = {},
-  -- if you don't use nvim-lspconfig you must pass your server name and
-  -- the related filetypes into this table
-  -- like server_filetype_map = { metals = { "sbt", "scala" } }
-  server_filetype_map = {},
 })
 
+local keymap = vim.keymap.set
 -- Lsp finder find the symbol definition implement reference
 -- if there is no implement it will hide
 -- when you use action in finder like open vsplit then you can
 -- use <C-t> to jump back
-keymap("n", "<leader>llf", "<Cmd>Lspsaga lsp_finder<CR>", opts)
+keymap("n", "<leader>llf", "<Cmd>Lspsaga lsp_finder<CR>")
 
 -- Code action
-keymap({ "n", "v" }, "<leader>lc", "<cmd>Lspsaga code_action<CR>", { silent = true })
+keymap({ "n", "v" }, "<leader>lca", "<cmd>Lspsaga code_action<CR>")
 
 -- Rename
-keymap("n", "<leader>lrn", "<cmd>Lspsaga rename<CR>", opts)
+keymap("n", "<leader>lrn", "<cmd>Lspsaga rename<CR>")
+
+-- Rename word in whole project
+keymap("n", "<leader>lrp", "<cmd>Lspsaga rename ++project<CR>")
 
 -- Peek Definition
--- you can edit the definition file in this flaotwindow
+-- you can edit the definition file in this float window
 -- also support open/vsplit/etc operation check definition_action_keys
 -- support tagstack C-t jump back
-keymap("n", "<leader>lp", "<cmd>Lspsaga peek_definition<CR>", { silent = true })
+keymap("n", "<leader>lp", "<cmd>Lspsaga peek_definition<CR>")
 
--- Show line diagnostics
-keymap("n", "<leader>lld", "<cmd>Lspsaga show_line_diagnostics<CR>", { silent = true })
+-- Go to Definition
+keymap("n", "<leader>lgd", "<cmd>Lspsaga goto_definition<CR>")
+
+-- Show line diagnostics you can pass argument ++unfocus to make
+-- show_line_diagnostics float window unfocus
+keymap("n", "<leader>lsl", "<cmd>Lspsaga show_line_diagnostics<CR>")
 
 -- Show cursor diagnostic
-keymap("n", "<leader>lcd", "<cmd>Lspsaga show_cursor_diagnostics<CR>", { silent = true })
+-- also like show_line_diagnostics  support pass ++unfocus
+keymap("n", "<leader>lsc", "<cmd>Lspsaga show_cursor_diagnostics<CR>")
 
--- Diagnsotic jump can use `<c-o>` to jump back
-keymap("n", "<leader>l[", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { silent = true })
-keymap("n", "<leader>l]", "<cmd>Lspsaga diagnostic_jump_next<CR>", { silent = true })
+-- Show buffer diagnostic
+keymap("n", "<leader>lsb", "<cmd>Lspsaga show_buf_diagnostics<CR>")
 
--- Only jump to error
+-- Diagnostic jump can use `<c-o>` to jump back
+keymap("n", "<leader>l[", "<cmd>Lspsaga diagnostic_jump_prev<CR>")
+keymap("n", "<leader>l]", "<cmd>Lspsaga diagnostic_jump_next<CR>")
+
+-- Diagnostic jump with filter like Only jump to error
 keymap("n", "[E", function()
-  require("lspsaga.diagnostic").goto_prev({ severity = vim.diagnostic.severity.ERROR })
-end, { silent = true })
+  require("lspsaga.diagnostic"):goto_prev({ severity = vim.diagnostic.severity.ERROR })
+end)
 keymap("n", "]E", function()
-  require("lspsaga.diagnostic").goto_next({ severity = vim.diagnostic.severity.ERROR })
-end, { silent = true })
+  require("lspsaga.diagnostic"):goto_next({ severity = vim.diagnostic.severity.ERROR })
+end)
 
--- Outline
-keymap("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", { silent = true })
+-- Toggle Outline
+keymap("n", "<leader>lo", "<cmd>Lspsaga outline<CR>")
 
 -- Hover Doc
-keymap("n", "<leader>lh", "<cmd>Lspsaga hover_doc<CR>", opts)
+-- if there has no hover will have a notify no information available
+-- to disable it just Lspsaga hover_doc ++quiet
+-- press twice it will jump into hover window
+keymap("n", "<leader>lh", "<cmd>Lspsaga hover_doc<CR>")
+-- if you want keep hover window in right top you can use ++keep arg
+-- notice if you use hover with ++keep you press this keymap it will
+-- close the hover window .if you want jump to hover window must use
+-- wincmd command <C-w>w
+keymap("n", "<leader>lh", "<cmd>Lspsaga hover_doc ++keep<CR>")
+-- keymap("n", "K", "<cmd>Lspsaga hover_doc ++keep<CR>")
+
+-- Callhierarchy
+keymap("n", "<Leader>lci", "<cmd>Lspsaga incoming_calls<CR>")
+keymap("n", "<Leader>lco", "<cmd>Lspsaga outgoing_calls<CR>")
 
 -- Float terminal
-keymap("n", "<leader>df", "<cmd>Lspsaga open_floaterm<CR>", { silent = true })
--- if you want pass somc cli command into terminal you can do like this
--- open lazygit in lspsaga float terminal
-keymap("n", "<leader>df", "<cmd>Lspsaga open_floaterm<CR>", { silent = true })
--- close floaterm
-keymap("t", "<leader>df", [[<C-\><C-n><cmd>Lspsaga close_floaterm<CR>]], { silent = true })
+keymap({ "n", "t" }, "<C-d>", "<cmd>Lspsaga term_toggle<CR>")
