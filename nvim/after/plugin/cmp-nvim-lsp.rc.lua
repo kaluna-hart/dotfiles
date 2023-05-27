ea9596132ba58a317f4e4772435582a09b731f23
Kumazawa <dev@rakuweb.jp>
2023-05-28 03:37:03 +0900

    feat:sparkles: update nvim-cmp

--- nvim/after/plugin/cmp-nvim-lsp.rc.lua
+++
@@ -1,9 +0,0 @@
-require("cmp").setup({
-	sources = {
-		{ name = "nvim_lsp" },
-	},
-})
-
--- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
