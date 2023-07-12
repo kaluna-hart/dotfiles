ac4ec157d5bdb7a57819bd8d2852160475cdf4a5
Kumazawa <dev@rakuweb.jp>
2023-07-12 23:42:32 +0900

    feat:sparkles: copilotを追加

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -125,4 +125,23 @@ packer.startup(function(use)
     end,
   })
   use({ "folke/todo-comments.nvim", requires = { "nvim-lua/plenary.nvim" } })
+  use({
+  "github/copilot.vim",
+  config = function()
+    vim.g.copilot_no_tab_map = true
+
+    local keymap = vim.keymap.set
+    -- https://github.com/orgs/community/discussions/29817#discussioncomment-4217615
+    keymap(
+      "i",
+      "<C-c>",
+      'copilot#Accept("<CR>")',
+      { silent = true, expr = true, script = true, replace_keycodes = false }
+    )
+    keymap("i", "<C-j>", "<Plug>(copilot-next)")
+    keymap("i", "<C-k>", "<Plug>(copilot-previous)")
+    keymap("i", "<C-o>", "<Plug>(copilot-dismiss)")
+    keymap("i", "<C-s>", "<Plug>(copilot-suggest)")
+  end,
+  })
 end)
