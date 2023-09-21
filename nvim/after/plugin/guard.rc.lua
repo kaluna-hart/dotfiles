4cb0daf15705f9f9381c9b05b1dc1705347c17b4
Kaluna Hart <37044099+kaluna-hart@users.noreply.github.com>
2023-09-22 01:49:41 +0900

    guard config修正 (#10)
    * feat:sparkles: rtx追加
    
    * feat:sparkles: rtx用configを追加
    
    * feat:sparkles:  nvimプラグインの整理
    
    * feat;sparkles: guard config修正
    
    ---------
    
    Co-authored-by: Kumazawa <dev@rakuweb.jp>

--- nvim/after/plugin/guard.rc.lua
+++ nvim/after/plugin/guard.rc.lua
@@ -1,14 +1,14 @@
 local ft = require("guard.filetype")
-ft("lua"):fmt("stylua")
+ft("lua"):fmt("lsp"):append("stylua")
 ft("typescript,javascript,typescriptreact"):fmt("prettier")
-ft('python'):fmt('black')
-ft('rust'):fmt('rustfmt')
+ft("python"):fmt("black")
+ft("rust"):fmt("rustfmt")
 
 require("guard").setup({
-  -- the only options for the setup function
-  fmt_on_save = false,
-  -- Use lsp if no formatter was defined for this filetype
-  lsp_as_default_formatter = false,
+	-- the only options for the setup function
+	fmt_on_save = false,
+	-- Use lsp if no formatter was defined for this filetype
+	lsp_as_default_formatter = false,
 })
 
 vim.keymap.set({ "n", "v" }, "<leader>gf", "<cmd>GuardFmt<CR>")
