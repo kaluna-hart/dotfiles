cb41f27d5ef7609f89dba16eb5f2800b0505b429
Kumazawa <dev@rakuweb.jp>
2023-09-23 10:03:37 +0900

    feat:sparkles: guard config更新

--- nvim/after/plugin/guard.rc.lua
+++ nvim/after/plugin/guard.rc.lua
@@ -3,12 +3,13 @@ ft("lua"):fmt("lsp"):append("stylua")
 ft("typescript,javascript,typescriptreact"):fmt("prettier")
 ft("python"):fmt("black")
 ft("rust"):fmt("rustfmt")
+ft("graphql"):fmt("prettier")
 
 require("guard").setup({
-	-- the only options for the setup function
-	fmt_on_save = false,
-	-- Use lsp if no formatter was defined for this filetype
-	lsp_as_default_formatter = false,
+  -- the only options for the setup function
+  fmt_on_save = false,
+  -- Use lsp if no formatter was defined for this filetype
+  lsp_as_default_formatter = false,
 })
 
 vim.keymap.set({ "n", "v" }, "<leader>gf", "<cmd>GuardFmt<CR>")
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,128 +1,134 @@
 return {
-  { "norcalli/nvim-colorizer.lua" },
-  { "marko-cerovac/material.nvim" },
-  {
-    "bluz71/vim-moonfly-colors",
-    name = "moonfly",
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    "nvimdev/guard.nvim",
-    dependencies = { "nvimdev/guard-collection" },
-  },
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-  },
-  { "nvim-lua/plenary.nvim" },
-  { "neovim/nvim-lspconfig" },
-  { "MunifTanjim/prettier.nvim" },
-  { "williamboman/mason.nvim" },
-  { "williamboman/mason-lspconfig.nvim" },
-  {
-    "nvimdev/lspsaga.nvim",
-    dependencies = { "nvim-lspconfig" },
-    config = function()
-      require("lspsaga").setup({
-        lightbulb = {
-          enable = false,
-          enable_in_insert = true,
-          sign = true,
-          sign_priority = 40,
-          virtual_text = true,
-        },
-      })
-    end,
-  },
+	{ "norcalli/nvim-colorizer.lua" },
+	{ "marko-cerovac/material.nvim" },
+	{
+		"bluz71/vim-moonfly-colors",
+		name = "moonfly",
+		lazy = false,
+		priority = 1000,
+	},
+	{
+		"nvimdev/guard.nvim",
+		dependencies = { "nvimdev/guard-collection" },
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	{ "nvim-lua/plenary.nvim" },
+	{ "neovim/nvim-lspconfig" },
+	{ "MunifTanjim/prettier.nvim" },
+	{ "williamboman/mason.nvim" },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = {
+			{ "williamboman/mason.nvim" },
+			{ "neovim/nvim-lspconfig" },
+		},
+	},
+	{
+		"nvimdev/lspsaga.nvim",
+		dependencies = { "nvim-lspconfig" },
+		config = function()
+			require("lspsaga").setup({
+				lightbulb = {
+					enable = false,
+					enable_in_insert = true,
+					sign = true,
+					sign_priority = 40,
+					virtual_text = true,
+				},
+			})
+		end,
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = function()
-      local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-      ts_update()
-    end,
-  },
-  { "nvim-telescope/telescope.nvim" },
-  { "nvim-telescope/telescope-file-browser.nvim" },
-  {
-    "AckslD/nvim-neoclip.lua",
-    dependencies = { "tami5/sqlite.lua" },
-  },
-  { "onsails/lspkind.nvim" },
-  { "nvim-tree/nvim-tree.lua" },
-  { "numToStr/Comment.nvim" },
-  { "windwp/nvim-ts-autotag" },
-  { "lukas-reineke/indent-blankline.nvim" },
-  {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-  },
-  {
-    "akinsho/bufferline.nvim",
-    version = "*",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-  },
-  { "xiyaowong/nvim-transparent" },
-  { "machakann/vim-sandwich" },
-  { "andymass/vim-matchup" },
-  { "liuchengxu/vista.vim" },
-  { "cohama/lexima.vim" },
-  { "tpope/vim-repeat" },
-  { "lewis6991/gitsigns.nvim" },
-  {
-    "dinhhuy258/git.nvim",
-  },
-  {
-    "hrsh7th/nvim-cmp",
-  },
-  { "hrsh7th/cmp-nvim-lsp" },
-  { "hrsh7th/cmp-buffer" },
-  { "hrsh7th/cmp-path" },
-  { "hrsh7th/cmp-cmdline" },
-  { "L3MON4D3/LuaSnip" },
-  { "saadparwaiz1/cmp_luasnip" },
-  { "ray-x/cmp-treesitter" },
-  "rafamadriz/friendly-snippets",
-  "j-hui/fidget.nvim",
-  {
-    "rust-lang/rust.vim",
-    ft = { "rust" },
-    config = function()
-      vim.g.rustfmt_autosave = 1
-    end,
-  },
-  {
-    "styled-components/vim-styled-components",
-    ft = { "javascript", "javascript.jsx", "javascriptreact", "typescript", "typescript.tsx", "typescriptreact" },
-    config = function()
-      vim.cmd([[source ~/.config/nvim/dein/plugins/vim-styled-components.plug.vim]])
-    end,
-  },
-  {
-    "phaazon/hop.nvim",
-    branch = "v2",
-    config = function()
-      require("hop").setup({ key = "etovxqpdygfblzhckisuran" })
-    end,
-  },
-  { "folke/todo-comments.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup({
-        suggestion = { enabled = false },
-        panel = { enabled = false },
-      })
-    end,
-  },
-  {
-    "zbirenbaum/copilot-cmp",
-    dependencies = { "copilot.lua" },
-    config = function()
-      require("copilot_cmp").setup()
-    end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = function()
+			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+			ts_update()
+		end,
+	},
+	{ "nvim-telescope/telescope.nvim" },
+	{ "nvim-telescope/telescope-file-browser.nvim" },
+	{
+		"AckslD/nvim-neoclip.lua",
+		dependencies = { "tami5/sqlite.lua" },
+	},
+	{ "onsails/lspkind.nvim" },
+	{ "nvim-tree/nvim-tree.lua" },
+	{ "numToStr/Comment.nvim" },
+	{ "windwp/nvim-ts-autotag" },
+	{ "lukas-reineke/indent-blankline.nvim" },
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+	},
+	{
+		"akinsho/bufferline.nvim",
+		version = "*",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	{ "xiyaowong/nvim-transparent" },
+	{ "machakann/vim-sandwich" },
+	{ "andymass/vim-matchup" },
+	{ "liuchengxu/vista.vim" },
+	{ "cohama/lexima.vim" },
+	{ "tpope/vim-repeat" },
+	{ "lewis6991/gitsigns.nvim" },
+	{
+		"dinhhuy258/git.nvim",
+	},
+	{
+		"hrsh7th/nvim-cmp",
+	},
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/cmp-buffer" },
+	{ "hrsh7th/cmp-path" },
+	{ "hrsh7th/cmp-cmdline" },
+	{ "L3MON4D3/LuaSnip" },
+	{ "saadparwaiz1/cmp_luasnip" },
+	{ "ray-x/cmp-treesitter" },
+	"rafamadriz/friendly-snippets",
+	"j-hui/fidget.nvim",
+	{
+		"rust-lang/rust.vim",
+		ft = { "rust" },
+		config = function()
+			vim.g.rustfmt_autosave = 1
+		end,
+	},
+	{
+		"styled-components/vim-styled-components",
+		ft = { "javascript", "javascript.jsx", "javascriptreact", "typescript", "typescript.tsx", "typescriptreact" },
+		config = function()
+			vim.cmd([[source ~/.config/nvim/dein/plugins/vim-styled-components.plug.vim]])
+		end,
+	},
+	{
+		"phaazon/hop.nvim",
+		branch = "v2",
+		config = function()
+			require("hop").setup({ key = "etovxqpdygfblzhckisuran" })
+		end,
+	},
+	{ "folke/todo-comments.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({
+				suggestion = { enabled = false },
+				panel = { enabled = false },
+			})
+		end,
+	},
+	{
+		"zbirenbaum/copilot-cmp",
+		dependencies = { "copilot.lua" },
+		config = function()
+			require("copilot_cmp").setup()
+		end,
+	},
 }
