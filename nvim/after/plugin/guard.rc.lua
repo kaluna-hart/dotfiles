29de4f53c1cbb5ed70778c6fc0f7d9ea99addca3
Kaluna Hart <37044099+kaluna-hart@users.noreply.github.com>
2023-10-13 01:04:25 +0900

    ibl,guard.nvimのconfig修正 (#12)
    * feat:sparkles: guard.nvimのconfig追加
    
    * feat:sparkles: guard.nvimのconfig修正
    
    * feat:sparkles:indent-blanklineのconfig修正
    
    ---------
    
    Co-authored-by: Kumazawa <dev@rakuweb.jp>

--- nvim/after/plugin/guard.rc.lua
+++ nvim/after/plugin/guard.rc.lua
@@ -1,4 +1,5 @@
 local ft = require("guard.filetype")
+
 ft("lua"):fmt("lsp"):append("stylua")
 ft("typescript,javascript,typescriptreact"):fmt("prettier")
 ft("python"):fmt("black")
@@ -9,7 +10,7 @@ require("guard").setup({
   -- the only options for the setup function
   fmt_on_save = false,
   -- Use lsp if no formatter was defined for this filetype
-  lsp_as_default_formatter = false,
+  lsp_as_default_formatter = true,
 })
 
 vim.keymap.set({ "n", "v" }, "<leader>gf", "<cmd>GuardFmt<CR>")
