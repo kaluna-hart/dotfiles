6345ca3cf11b35ebe20d82d6d8870ca56cc9b010
Kumazawa <dev@rakuweb.jp>
2024-06-30 22:53:01 +0900

    feat:+1: gitsigns config修正

--- nvim/after/plugin/gitsigns.rc.lua
+++ nvim/after/plugin/gitsigns.rc.lua
@@ -1,7 +1,7 @@
 require("gitsigns").setup({
 	signs = {
-		add = { text = "│" },
-		change = { text = "│" },
+		add = { text = "┃" },
+		change = { text = "┃" },
 		delete = { text = "_" },
 		topdelete = { text = "‾" },
 		changedelete = { text = "~" },
@@ -14,7 +14,8 @@ require("gitsigns").setup({
 	watch_gitdir = {
 		follow_files = true,
 	},
-	attach_to_untracked = true,
+	auto_attach = true,
+	attach_to_untracked = false,
 	current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
 	current_line_blame_opts = {
 		virt_text = true,
@@ -23,7 +24,7 @@ require("gitsigns").setup({
 		ignore_whitespace = false,
 		virt_text_priority = 100,
 	},
-	current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+	current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
 	sign_priority = 6,
 	update_debounce = 100,
 	status_formatter = nil, -- Use default
@@ -35,8 +36,5 @@ require("gitsigns").setup({
 		relative = "cursor",
 		row = 0,
 		col = 1,
-	},
-	yadm = {
-		enable = false,
 	},
 })
