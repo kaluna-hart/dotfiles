7ddff9b7464419bf970498be2d513ff24f7c27b6
Kumazawa <dev@rakuweb.jp>
2026-05-05 04:26:10 +0900

    feat:sparkles: fzfの不要コードを削除

--- fzf/fzf.zsh
+++ fzf/fzf.zsh
@@ -1,16 +1,12 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == ${HOME}/.local/share/mise/installs/fzf/0.65.1/bin* ]]; then
-  # Key bindings
-  source "${HOME}/.local/share/mise/installs/fzf/0.65.1/shell/key-bindings.zsh"
-# Auto-completion
-  source "${HOME}/.local/share/mise/installs/fzf/0.65.1/shell/completion.zsh"
+if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
 fi
+
+source <(fzf --zsh)
 
 export FZF_CTRL_T_COMMAND="rg --files --hidden --follow --glob '!.git/*' 2>/dev/null"
 export FZF_CTRL_T_OPTS='--height 80% --border --layout=reverse --preview "bat --color=always --line-range :100 {}"'
-# export FZF_CTRL_T_OPTS='--height 80% --border --layout=reverse --preview "bat --color=always --style=header,grid --line-range :100 {}"'
-# use ripgrep
 export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*"'
 export FZF_DEFAULT_OPTS='--height 80% --border --layout=reverse --preview "bat --color=always --line-range :100 {}"'
-# export FZF_DEFAULT_OPTS='--height 80% --border --layout=reverse --preview "bat --color=always --style=header,grid --line-range :100 {}"'
